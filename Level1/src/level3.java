279ac9f61cd1c5a31611df6a77e351bd8296381b
siniberry <1031cory@naver.com>
2022-07-05 23:58:39 +0900

    2022-07-05

---
+++ Level1/src/level3.java
@@ -0,0 +1,15 @@
+
+public class level3 {
+	public static void main(String[] args) 
+	{
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
+
+
+
+
