8c95410ba4adf0e99aae8a72b37bbc380ee921d1
siniberry <1031cory@naver.com>
2022-07-06 23:55:33 +0900

    2022.07.06

--- Baekjoon/src/Level1/level1.java
+++ Baekjoon/src/Level1/level1.java
@@ -1,3 +1,4 @@
+package Level1;
 
 public class level1 {
 	public static void main(String[] args) {
--- Baekjoon/src/Level1/level2.java
+++ Baekjoon/src/Level1/level2.java
@@ -1,3 +1,4 @@
+package Level1;
 
 public class level2 {
 	public static void main(String[] args) {
