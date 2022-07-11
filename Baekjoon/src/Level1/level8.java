afd4c1e3dad1933fffa957b4165f4ca1d686bc47
siniberry <1031cory@naver.com>
2022-07-11 19:02:25 +0900

    2022-07-10

---
+++ Baekjoon/src/Level1/level8.java
@@ -0,0 +1,19 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class level8 {
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		double a, b;
+		
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+
+		System.out.println(a / b);
+	}
+}
+
+
+
