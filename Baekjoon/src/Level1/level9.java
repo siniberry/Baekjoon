4b3125de1ab407ac71bf7f04239f5c653d78c5be
siniberry <1031cory@naver.com>
2022-07-11 19:04:57 +0900

    2022-07-11

---
+++ Baekjoon/src/Level1/level9.java
@@ -0,0 +1,23 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class level9 {
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+}
+
+
+
