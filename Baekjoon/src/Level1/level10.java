dbbf36dcb596db0b61c88dbd05b135be3dc49934
siniberry <1031cory@naver.com>
2022-07-13 13:59:11 +0900

    2022-07-12

---
+++ Baekjoon/src/Level1/level10.java
@@ -0,0 +1,16 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class level10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a;
+		
+		a = sc.next();
+		
+		System.out.println(a + "??!");
+	}
+
+}
