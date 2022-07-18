445bc599a65f1b1970726e40ce56c0ab7f507222
siniberry <1031cory@naver.com>
2022-07-19 00:06:17 +0900

    2022-07-18

---
+++ Baekjoon/src/Level1/level14.java
@@ -0,0 +1,15 @@
+package Level1;
+
+public class level14 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+
+	}
+}
